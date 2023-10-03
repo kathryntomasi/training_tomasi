2641c3e5a6f47ab39d17212c2fdb37a548a31681
kathryntomasi <kathryntomasi@ucsb.edu>
2023-10-03 10:20:20 -0700

    Initial commit

---
+++ R/intro-r-programming.R
@@ -0,0 +1,62 @@
+
+
+# use pound to write comments ----
+3+4
+
+# data viz ----
+#shortcut for creating sections in R script cmd + shift + R
+
+# spatial analysis --------------------------------------------------------
+
+# R calculations with objects
+
+# R calculations ----------------------------------------------------------
+
+#option + - creates assignment operator (<-)
+
+weight_kg <- c(55, 25, 12)
+
+weight_lb <- 2.2 * weight_kg
+
+# string data type 
+
+# string data type --------------------------------------------------------
+
+science_rocks <- "Yes it does!"
+
+# creating vectors 
+
+# creating vectors --------------------------------------------------------
+
+mixed_vec <- c(5, "science", TRUE)
+
+# creating lists 
+
+# lists -------------------------------------------------------------------
+
+mixed_list <- list(5, "science", TRUE)
+
+# use mean function 
+
+# functions 
+
+# functions ---------------------------------------------------------------
+
+weight_mean <- mean(x = weight_lb)
+
+# use read.csv() to read in data ----
+
+bg_chem_dat <- read.csv("data/BGchem2008data.csv")
+
+# use subset operator ($) ----
+
+head(bg_chem_dat$Station)
+
+mean(bg_chem_dat$CTD_Temperature)
+
+# literate analysis 
+
+# literary analysis -------------------------------------------------------
+
+
+
